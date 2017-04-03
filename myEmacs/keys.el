d80dd8526e21dab61eb02d9823445c4ea1410489
William Hexberg <whexberg@gmail.com>
2017-04-03 16:56:23 -0700

    added helm keybindings

--- myEmacs/keys.el
+++ myEmacs/keys.el
@@ -5,4 +5,13 @@
   (global-set-key (kbd "S-M-<up>") 'move-region-up)
   (global-set-key (kbd "S-M-<down>") 'move-region-down)
 
-  (global-set-key (kbd "C-\\") 'neotree-toggle))
+  (global-set-key (kbd "C-\\") 'neotree-toggle)
+
+  (global-set-key (kbd "M-x") 'helm-M-x)
+  (global-set-key (kbd "C-x b") 'helm-mini)
+  (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+  (global-set-key (kbd "C-x C-f") 'helm-find-files)
+  (global-set-key (kbd "C-x C-r") 'helm-recentf)
+  (global-set-key (kbd "M-y") 'helm-show-kill-ring)
+
+  )
